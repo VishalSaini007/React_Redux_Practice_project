88174fe98c97d6054eb63e76c540b65a8333f464
vishal saini <vishalsaini9582@gmail.com>
2024-02-09 17:40:49 +0530

    designed our basic  cart

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import '../App.css'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <>
+        <div>Home</div>
+
+        <div className='cart'>
+            <h1>Cart : 0 </h1>
+        </div>
+      
+
+        <div className='item'>
+            <h1>Samsung</h1>
+            <img width={100} height={200} src="https://i.ebayimg.com/00/s/MTUwMFgxMTY3/z/B~MAAOSwyhtjD-Ws/$_57.JPG?set_id=8800005007" alt="phone image" />
+            <span>Price : 45,000 Rs</span>
+            <button >Add to Cart</button>
+
+        </div>
+    </>
+
   )
 }
